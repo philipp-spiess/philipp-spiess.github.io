17e47d4b46c1f68a0ea56deb31d78c1f7ad7bb51
Philipp Spiess <hello@philippspiess.com>
2017-06-06 11:36:57 +0200

    Add worker example

---
+++ dev/wasm-hello-world/worker.js
@@ -0,0 +1,22 @@
+var Module = {
+  preRun: [],
+  postRun: [],
+  print: (function() {
+    return function(text) {
+      if (arguments.length > 1)
+        text = Array.prototype.slice.call(arguments).join(" ");
+      console.log(text);
+    };
+  })(),
+  printErr: function(text) {
+    if (arguments.length > 1)
+      text = Array.prototype.slice.call(arguments).join(" ");
+    if (0) {
+      // XXX disabled for safety typeof dump == 'function') {
+      dump(text + "\n"); // fast, straight to the real console
+    } else {
+      console.error(text);
+    }
+  },
+};
+self.importScripts('hello.js');
